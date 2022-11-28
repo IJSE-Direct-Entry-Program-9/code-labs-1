648615cad6b05232b4508bb7ac37646e0a67237d
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-11-28 10:30:26 +0530

    Add model-mapper

---
+++ model-mapper/src/main/java/lk/ijse/dep9/util/Converter.java
@@ -0,0 +1,25 @@
+package lk.ijse.dep9.util;
+
+import lk.ijse.dep9.dto.CustomerDTO;
+import lk.ijse.dep9.dto.OrderDTO;
+import lk.ijse.dep9.dto.OrderDetailDTO;
+import lk.ijse.dep9.entity.Customer;
+import lk.ijse.dep9.entity.Order;
+import lk.ijse.dep9.entity.OrderDetail;
+
+import java.util.List;
+
+public class Converter {
+
+    public Customer toCustomer(CustomerDTO customerDTO){
+        return null;
+    }
+
+    public Order toOrder(OrderDTO orderDTO){
+        return null;
+    }
+
+    public List<OrderDetail> toOrderDetailList(OrderDTO orderDTO){
+        return null;
+    }
+}
